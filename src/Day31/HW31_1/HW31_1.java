7c4df492309379b3e3a979e63863c8058f38282d
Muha <muhtarkasimov@gmail.com>
2019-11-30 07:15:17 +0600

    HW31_1 Primitive selecting script written (demo)

--- src/Day31/HW31_1/HW31_1.java
+++ src/Day31/HW31_1/HW31_1.java
@@ -6,9 +6,15 @@ public class HW31_1 {
 
         Turtle t1 = new Turtle("Leonardo", 15, 100);
         Duck d1 = new Duck("Pascal", 14, 125);
-        Shark s1 = new Shark("Arab", 10, 75);
+        Shark s1 = new Shark("Arab", 13, 75);
 
         SelectAnimal[] animals = {t1, d1, s1,};
 
+        // selecting by
+        for (SelectAnimal sa : animals) {
+            if (sa instanceof Shark && ((Shark)sa).getAge() > 10) {
+                System.out.println(((Shark) sa).getName() + " is shark and he is over than 10 years old)");
+            }
+        }
     }
 }
