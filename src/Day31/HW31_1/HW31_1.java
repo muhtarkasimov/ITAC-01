0670404ec503cd48fce172e8d3266fc2d48bf313
Muha <muhtarkasimov@gmail.com>
2019-11-30 07:37:55 +0600

    HW31_1 Added AnimalArrayInit method: creates new SelectAnimal[] array; fills it with random vars in given bounds; Test are not yet performed

--- src/Day31/HW31_1/HW31_1.java
+++ src/Day31/HW31_1/HW31_1.java
@@ -1,14 +1,13 @@
 package Day31.HW31_1;
 
+import java.util.Random;
+
 public class HW31_1 {
 
     public static void main(String[] args) {
 
-        Turtle t1 = new Turtle("Leonardo", 15, 100);
-        Duck d1 = new Duck("Pascal", 14, 125);
-        Shark s1 = new Shark("Arab", 13, 75);
+        SelectAnimal[] animals = initAnimalsArray(100,1,20,50,500);
 
-        SelectAnimal[] animals = {t1, d1, s1,};
 
         // selecting by
         for (SelectAnimal sa : animals) {
@@ -16,5 +15,29 @@ public class HW31_1 {
                 System.out.println(((Shark) sa).getName() + " is shark and he is over than 10 years old)");
             }
         }
+    }
+
+    public static SelectAnimal[] initAnimalsArray(int size,
+                                                  int ageMinBound,
+                                                  int ageMaxBound,
+                                                  int weightMinBound,
+                                                  int weightMaxBound) {
+        SelectAnimal[] animals = new SelectAnimal[size];
+        Random random = new Random();
+        for (int i = 0; i < animals.length ; i++) {
+            //random vars
+            String randomName = "" +  (char)random.nextInt((122 - 97 + 1) + 97) + (char)random.nextInt((122 - 97 + 1) + 97);
+            int randomAge = random.nextInt((ageMaxBound - ageMinBound + 1) + ageMinBound);
+            int randomWeight = random.nextInt((weightMaxBound - weightMinBound + 1) + weightMinBound);
+
+            switch (random.nextInt(3)) {
+                case 0: animals[i] = new Turtle(randomName,randomAge,randomWeight);
+                case 1: animals[i] = new Shark(randomName,randomAge,randomWeight);
+                case 2: animals[i] = new Duck(randomName,randomAge,randomWeight);
+            }
+        }
+
+
+        return animals;
     }
 }
