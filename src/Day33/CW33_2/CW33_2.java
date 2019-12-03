de34d0b724b748e47f5669b9df17e2be74eae8c2
Mukhtar Kasimov <muhtarkasimov@gmail.com>
2019-12-03 19:59:10 +0600

    Day33.CW33_2 Catching outOfBound exception

--- src/Day33/CW33_2/CW33_2.java
+++ src/Day33/CW33_2/CW33_2.java
@@ -3,8 +3,11 @@ package Day33.CW33_2;
 public class CW33_2 {
 
     public static void main(String[] args) throws Exception {
+        //there are two versions of this file on GitHub
+        //first one was catching nullPointer exception
+        //this one is catching outOfBound exception
 
-        Cat[] cats = {new Cat("Murzik"), null};
+        Cat[] cats = {new Cat("Murzik"), new Cat("Bobik")};
 
         int i = 0;
         try {
