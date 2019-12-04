fc7746c28ef1ce2c9d48411df699e4d8d609fb9f
Mukhtar Kasimov <muhtarkasimov@gmail.com>
2019-12-04 20:52:29 +0600

    Day33.HW33_1 noSpaceFile added writes the same but without spaces

--- src/Day33/HW33_1/HW33_1.java
+++ src/Day33/HW33_1/HW33_1.java
@@ -9,30 +9,39 @@ public class HW33_1 {
         try {
             File existingFile = new File("./src/Day33/HW33_1/HW33_1.java");
             File newFile = new File("./src/Day33/HW33_1/newFile.java");
+            File noSpaceFile = new File("./src/Day33/HW33_1/noSpaceFile.txt");
+
+            FileWriter noSpaceWriter = new FileWriter(noSpaceFile);
             FileWriter fw = new FileWriter(newFile);
             FileReader fr = new FileReader(existingFile);
             Scanner scan = new Scanner(fr);
 
+            String line;
             while (scan.hasNextLine()) {
-                fw.write("//" + scan.nextLine() + "\n");
+                line = scan.nextLine();
+                fw.write("//" + line + "\n");
+                noSpaceWriter.write("//" + line.trim() + "\n");
             }
+            //trim() don't works with comments done by [ctrl] + [/]
+            //may be it will work if do trim() twice, but i don't checked it
             //adding comment signs to not to confuse my intelliJ
 
-//            System.out.println("incode works");
+            //System.out.println("incode works");
 
             fw.close();
             fr.close();
             scan.close();
+            noSpaceWriter.close();
 
             if (newFile.length() > 100) {
                 throw new IllegalStateException("newFile.java code is too big (more than 100 bytes)");
             }
         } catch (FileNotFoundException fnfe) {
             fnfe.printStackTrace();
-//        } catch (Exception e) {
-//            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         } finally {
-//            System.out.println("Finally works");
+            //System.out.println("Finally works");
         }
     }
 }
