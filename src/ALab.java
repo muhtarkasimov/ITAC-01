7cca05b3f78f50a1cffc2e60cbd8d697bd7cde89
Mukhtar Kasimov <muhtarkasimov@gmail.com>
2019-12-04 20:19:43 +0600

    Day33.HW33_1 throwing size error exceptions is successful catching fileNotFound exceptions is successful

--- src/ALab.java
+++ src/ALab.java
@@ -1,11 +1,11 @@
+import java.io.*;
 import java.util.Random;
 
 public class ALab {
-    public static void main(String[] args) {
-        String s = "a";
-        String ss = null;
-
-        System.out.println(s == ss);
+    public static void main(String[] args) throws Exception {
+        File file = new File("./songFile.txt");
+        long len = file.length();
+        System.out.println(len);
     }
 }
 
--- src/Day33/HW33_1/HW33_1.java
+++ src/Day33/HW33_1/HW33_1.java
@@ -1,23 +1,38 @@
 package Day33.HW33_1;
 
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.*;
 import java.util.Scanner;
 
 public class HW33_1 {
 
     public static void main(String[] args) throws Exception {
-        FileWriter fw = new FileWriter("./src/Day33/HW33_1/newFile.java");
-        FileReader fr = new FileReader("./src/Day33/HW33_1/HW33_1.java");
-        Scanner scan = new Scanner(fr);
+        try {
+            File existingFile = new File("./src/Day33/HW33_1/HW33_1.java");
+            File newFile = new File("./src/Day33/HW33_1/newFile.java");
+            FileWriter fw = new FileWriter(newFile);
+            FileReader fr = new FileReader(existingFile);
+            Scanner scan = new Scanner(fr);
 
-        while (scan.hasNextLine()) {
-            fw.write("//" + scan.nextLine() + "\n");
+            while (scan.hasNextLine()) {
+                fw.write("//" + scan.nextLine() + "\n");
+            }
+            //adding comment signs to not to confuse my intelliJ
+
+//            System.out.println("incode works");
+
+            fw.close();
+            fr.close();
+            scan.close();
+
+            if (newFile.length() > 100) {
+                throw new IllegalStateException("newFile.java code is too big (more than 100 bytes)");
+            }
+        } catch (FileNotFoundException fnfe) {
+            fnfe.printStackTrace();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+        } finally {
+//            System.out.println("Finally works");
         }
-        //adding comment signs to not to confuse my intelliJ
-
-        fw.close();
-        fr.close();
-        scan.close();
     }
 }
