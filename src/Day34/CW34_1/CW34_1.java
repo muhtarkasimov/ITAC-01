e2e2159b85dd50ead43afa2c956220da5781409a
Mukhtar Kasimov <muhtarkasimov@gmail.com>
2019-12-05 19:54:49 +0600

    Day34.CW34_1 Discovering ArrayList

--- src/Day34/CW34_1/CW34_1.java
+++ src/Day34/CW34_1/CW34_1.java
@@ -8,11 +8,20 @@ public class CW34_1 {
 
         ArrayList<Integer> list = new ArrayList<>();
 
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 10000; i++) {
             if (Math.sqrt(i) % 1 == 0) {
                 list.add(i);
             }
         }
         System.out.println(list);
+
+        ArrayList<Integer> list2 = new ArrayList<>();
+        for (int i = 0; i < list.size() ; i++) {
+            if (list.get(i) % 2 == 0 || list.get(i) % 3 == 0) {
+                list2.add(list.indexOf(list.get(i)));
+            }
+        }
+        System.out.println(list2);
+
     }
 }
