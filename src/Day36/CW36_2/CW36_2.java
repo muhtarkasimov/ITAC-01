ae25e34428e7f8313270f515068771840dcd929d
Mukhtar Kasimov <muhtarkasimov@gmail.com>
2019-12-10 20:41:40 +0600

    Day36.CW36_2 switch realisation to compare enums

--- src/Day36/CW36_2/CW36_2.java
+++ src/Day36/CW36_2/CW36_2.java
@@ -4,14 +4,24 @@ public class CW36_2 {
 
     public static void main(String[] args) {
 
-        WeekDays day = WeekDays.Tuesday;
+        WeekDays day = WeekDays.Friday;
 
-        if (day.ordinal() == WeekDays.Monday.ordinal()
-                || day.ordinal() == WeekDays.Wednesday.ordinal()
-                || day.ordinal() == WeekDays.Friday.ordinal()) {
-            System.out.println("I am studying on " + day);
-        } else {
-            System.out.println("I am not studying on " + day);
+//        if (day.ordinal() == WeekDays.Monday.ordinal()
+//                || day.ordinal() == WeekDays.Wednesday.ordinal()
+//                || day.ordinal() == WeekDays.Friday.ordinal()) {
+//            System.out.println("I am studying on " + day);
+//        } else {
+//            System.out.println("I am not studying on " + day);
+//        }
+
+        switch (day) {
+            case Monday:
+            case Wednesday:
+            case Friday:
+                System.out.println("I am studying on " + day);
+                break;
+            default:
+                System.out.println("I am not studying on " + day);
         }
 
     }
