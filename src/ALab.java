8caf88096a6ef3e77732e2accc057f2e2f7baf79
Mukhtar Kasimov <muhtarkasimov@gmail.com>
2019-12-10 19:42:03 +0600

    Day34

--- src/ALab.java
+++ src/ALab.java
@@ -3,9 +3,7 @@ import java.util.Random;
 
 public class ALab {
     public static void main(String[] args) throws Exception {
-        File file = new File("./songFile.txt");
-        long len = file.length();
-        System.out.println(len);
+
     }
 }
 
---
+++ src/Day34/HW34_1/HW34_2.java
@@ -0,0 +1,8 @@
+package Day34.HW34_1;
+
+public class HW34_2 {
+
+    public static void main(String[] args) {
+
+    }
+}
