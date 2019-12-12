d87b85b190f3d832ad176dc70b898bb7d7e031db
Mukhtar Kasimov <muhtarkasimov@gmail.com>
2019-12-12 19:11:35 +0600

    CW36_2 WeekDay enum

--- src/Day36/CW36_2/WeekDays.java
+++ src/Day36/CW36_2/WeekDays.java
@@ -1,11 +1,21 @@
 package Day36.CW36_2;
 
 public enum WeekDays {
-    Monday,
-    Tuesday,
-    Wednesday,
-    Thursday,
-    Friday,
-    Saturday,
-    Sunday
+    Monday("Понедельник"),
+    Tuesday("Вторник"),
+    Wednesday("Среда"),
+    Thursday("Четверг"),
+    Friday("Птяница"),
+    Saturday("Суббота"),
+    Sunday("Воскресение");
+
+    private String rusName;
+
+    WeekDays(String rusName) {
+        this.rusName = rusName;
+    }
+
+    public String getRusName() {
+        return rusName;
+    }
 }
